8198687e594f0bf8d8318db2f7b6de21a518da29
Vinicius-MAfonso <69774312+Vinicius-MAfonso@users.noreply.github.com>
2022-06-13 13:59:32 -0300

    att

---
+++ PrimeiroCap/src/primeirocap/PrimeiroCap.java
@@ -0,0 +1,14 @@
+package primeirocap;
+
+
+public class PrimeiroCap {
+
+   public static void main(String[] args) {
+       System.out.printf("%s%n%s%n","Welcome to","Java Programming!");
+       System.out.printf("%s%n%n%s", "Welcome to the", "Jungle");
+       System.out.print("Hello world");
+       System.out.println("Hello world");
+       System.out.printf("Hello world");
+    }//Fim do método
+    
+}
