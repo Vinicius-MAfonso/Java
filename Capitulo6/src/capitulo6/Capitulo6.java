f31a9802541fade3c6325de8c8be5d5c0cbb99d2
Vinicius-MAfonso <69774312+Vinicius-MAfonso@users.noreply.github.com>
2022-07-04 13:59:47 -0300

    att

---
+++ Capitulo6/src/capitulo6/Capitulo6.java
@@ -0,0 +1,13 @@
+package capitulo6;
+import java.util.Scanner;
+
+public class Capitulo6 {
+    
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println(Math.sqrt(900.00));
+    
+        
+    }
+    
+}
