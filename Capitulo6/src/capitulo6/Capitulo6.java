874296a95d9eb0ad63c8904501638309de626794
Vinicius-MAfonso <69774312+Vinicius-MAfonso@users.noreply.github.com>
2022-07-06 13:58:20 -0300

    att

--- Capitulo6/src/capitulo6/Capitulo6.java
+++ Capitulo6/src/capitulo6/Capitulo6.java
@@ -7,9 +7,12 @@ public class Capitulo6 {
     Scanner input = new Scanner(System.in);
     System.out.println(Math.sqrt(900.00));
     
+    
     String frase = "Hello";
     frase += " World!";
     System.out.println(frase);
+    
+    System.out.println(Math.sqrt(5));
     }
     
     
