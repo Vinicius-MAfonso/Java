aa63ab460285ae830f842ad7e7539c51460bcff0
Vinicius-MAfonso <69774312+Vinicius-MAfonso@users.noreply.github.com>
2022-07-08 13:57:01 -0300

    att

--- Capitulo6/Capitulo6/src/capitulo6/Capitulo6.java
+++ Capitulo6/Capitulo6/src/capitulo6/Capitulo6.java
@@ -1,8 +1,8 @@
-package capitulo6;
+    package capitulo6;
 import java.util.Scanner;
 
 public class Capitulo6 {
-    
+    private static int x = 10;
     public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
     System.out.println(Math.sqrt(900.00));
@@ -13,7 +13,11 @@ public class Capitulo6 {
     System.out.println(frase);
     
     System.out.println(Math.sqrt(5));
+    
+    sum();
+    System.out.println(x);
     }
-    
-    
+    private static int sum(){
+        return x+=1;
+    }   
 }
