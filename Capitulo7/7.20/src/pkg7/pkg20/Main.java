456a71d92dd6ba18e025736af5509ea41d3c45e3
Vinicius Mariano Afonso <vinicius.mariano.afonso@gmail.com>
2022-07-27 13:57:05 -0300

    att

--- Capitulo7/7.20/src/pkg7/pkg20/Main.java
+++ Capitulo7/7.20/src/pkg7/pkg20/Main.java
@@ -1,33 +1,69 @@
 package pkg7.pkg20;
-/*
-Array Bidimensional!
-
-Uma empresa tem 4 equipes de venda
-que vendem 5 produtos, Uma vez por dia cada vendedor passa uma nota de cada produto
-vendido:
-*Número do vendedor
-*Número do Produto
-*Valor total em reais do produto vendido no dia
-Logo serão entre 0 a 5 notas por vendedor no dia...
-
-O aplicativo deverá ler todas as informações do mês e resumir as vendas 
-por vendedor e produto, os totais devem estar em um array bidimensional sales,
-depois de processar todos os dados exiba em formato tabular 
-com colunas representando
-vendedor particular
-e linhas um produto particular.
-
-A saída deve incluir totais cruzados a direita das linha totalizadas e na parte
-inferior das colunas totalizadas
-(Some cada linha para obter o total de vendas de cada produto no mes,
-Some cada coluno para obter o total de vendas por vendedor no ultimo mes)
+import java.util.Arrays;
+import java.util.Scanner;
 
 
-*/
 public class Main {
-
+private static final Scanner input = new Scanner(System.in);
+private static final int TAM_LINHA = 5;
+private static final int TAM_COLUNA = 4;
     public static void main(String[] args) {
+        int sales[][] = new int[TAM_LINHA][TAM_COLUNA];
+        int nVendedor;
+        int nProduto;
         
+        System.out.println("Sistema de vendas");
+        
+        while(isAvailable(sales)){
+            System.out.print("Digite o número de vendedor[1 - 4]: ");
+            nVendedor = input.nextInt()-1;
+            System.out.print("Digite o número do produto[1 - 5]: ");
+            nProduto = input.nextInt()-1;
+            System.out.print("Digite o valor total vendido desse produto:R$ ");
+            sales[nProduto][nVendedor] = input.nextInt();
+            
+            sales[TAM_LINHA-1][nProduto] += sales[nProduto][nVendedor];
+            sales[nVendedor][TAM_COLUNA-1] += sales[nProduto][nVendedor];
+            sales[TAM_LINHA-1][TAM_COLUNA-1] += sales[nProduto][nVendedor];
+        }
+        showData(sales); 
+    }
+    
+    private static boolean isAvailable(int[][] array){
+        for(int linha = 0;linha < TAM_LINHA-1;linha++){
+            for(int coluna = 0;coluna < TAM_COLUNA-1;coluna++){
+                if(array[linha][coluna] == 0){
+                    return true;
+                }
+            }
+        }
+        return false;  
+    }
+    
+    private static void showData(int[][] array){
+        System.out.println("Apresentando dados...");
+        System.out.print("         ");
+        
+        //Linha de vendedor
+        for(int i = 1;i <= TAM_COLUNA;i++){
+            if(i < TAM_COLUNA){
+                System.out.printf("%10s %d ","Vendedor",i);
+            }else{
+                System.out.println("Lucro produto");
+            }
+        }
+        
+        for(int linha = 0;linha < TAM_LINHA;linha++){
+            if(linha == TAM_LINHA-1){
+                System.out.print("Lucro vendedor");
+            }else{
+                System.out.printf("%s %d ","Produto",linha+1);
+            }
+            for(int coluna = 0;coluna < TAM_COLUNA;coluna++){
+                System.out.printf("%10d",array[linha][coluna]);
+            }
+            System.out.println("");
+        }
     }
     
 }
