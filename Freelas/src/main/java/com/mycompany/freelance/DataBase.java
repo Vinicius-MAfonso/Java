206fb9a73b141c19bee1ab373531f49435f6d2a3
Vinicius Mariano Afonso <69774312+Vinicius-MAfonso@users.noreply.github.com>
2022-11-10 17:16:04 -0300

    att

--- Freelas/src/main/java/com/mycompany/freelance/DataBase.java
+++
@@ -1,5 +0,0 @@
-package com.mycompany.freelance;
-
-public class DataBase {
-    
-}
--- Freelas/src/main/java/com/mycompany/freelance/Freelance.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.freelance;
-
-public class Freelance {
-
-    public static void main(String[] args) {
-        MainFrame mainFrame = new MainFrame();
-    }
-}
